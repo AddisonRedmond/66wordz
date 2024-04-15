0b75f6da76207be834fa95fcd4c411def97932d9
addison <addison@addisonredmond.com>
2024-04-15 18:27:17 -0500

    fixed grid responsiveness issue

--- src/components/challenge/challenge-board.tsx
+++ src/components/challenge/challenge-board.tsx
@@ -14,9 +14,11 @@ const ChallengeBoard: React.FC<ChallengeBoardProps> = (props) => {
       transition={{ duration: 0.15 }}
       className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-400 bg-opacity-50"
     >
-      <div className="flex sm:w-1/2 max-h-[90vh] flex-col gap-5 rounded-md bg-white p-5 items-center justify-center">
+
+      <div className="flex md:w-1/2 max-h-[90vh] flex-col gap-5 rounded-md bg-white p-5 items-center justify-center min-w-[360px]">
         {/* word container */}
-        <WordContainer word="PENIS" revealIndex={[1, 4, 3]} />
+        <h2 className="font-semibold text-2xl">Challenge</h2>
+        {/* <WordContainer word="PENIS" revealIndex={[1, 4, 3]} /> */}
         <GuessGrid />
         {/* keyboard */}
         <Keyboard disabled={false} handleKeyBoardLogic={()=>{}} />
--- src/components/challenge/guess-grid.tsx
+++ src/components/challenge/guess-grid.tsx
@@ -6,8 +6,8 @@ const WordRow = (props: { word?: string }) => {
     <div className="my-1 flex w-full flex-row items-center justify-center gap-1 rounded-md duration-150 ease-in-out h-1/5">
       {Array.from({ length: 5 }).map((_, index) => {
         return (
-          <div className="grid aspect-square bg-white w-1/5 place-content-center rounded-md border-2 border-zinc-400 text-[4vh] font-bold  duration-200 ease-in-out">
-            <p>{props.word? props.word.toUpperCase().split("")[index] : ""}</p>
+          <div className="grid aspect-square bg-white w-1/5 place-content-center rounded-md border-2 border-zinc-400 text-3xl font-bold  duration-200 ease-in-out">
+            <p className="h-fit">{props.word? props.word.toUpperCase().split("")[index] : ""}</p>
           </div>
         );
       })}
@@ -17,7 +17,7 @@ const WordRow = (props: { word?: string }) => {
 
 const GuessGrid = () => {
   return (
-    <div className="rounded-md bg-stone-300 py-2 min-w-[360px] px-3">
+    <div className="rounded-md bg-stone-300 py-2 px-3 min-h-80 min-w-72 max-w-96 w-full">
       <WordRow word="PLANE"/>
       <WordRow />
       <WordRow />
