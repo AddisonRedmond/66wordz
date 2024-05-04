e78cdd78ef618dea007bcf3e4a8e1aae4a7d8a9c
addison <addison@addisonredmond.com>
2024-05-04 08:35:40 -0500

    trying to fix sign in issues

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Tile from "~/components/tile";
 import { m } from "framer-motion";
 import Link from "next/link";
-import { SignInButton } from "@clerk/nextjs";
+// import { SignInButton } from "@clerk/nextjs";
 import { GetServerSideProps } from "next";
 import { getAuth, buildClerkProps, clerkClient } from "@clerk/nextjs/server";
 export default function Login() {
@@ -21,11 +21,11 @@ export default function Login() {
         </div>
         <div className="relative flex h-1/2 flex-col items-center justify-center gap-48 bg-white font-bold md:h-full md:w-1/2">
           <div className="flex items-center gap-10">
-            <SignInButton>
+            {/* <SignInButton> */}
               <button className=" rounded-md border-2 border-[#9462C6] bg-black px-4 text-[4vh] text-white duration-150 ease-in-out hover:bg-zinc-700">
                 SIGN IN
               </button>
-            </SignInButton>
+            {/* </SignInButton> */}
           </div>
           <div className=" absolute bottom-2 flex w-full justify-around text-sm text-zinc-600">
             <Link className="... w-1/5 truncate" href="terms-of-service">
