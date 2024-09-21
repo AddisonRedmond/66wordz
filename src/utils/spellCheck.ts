b1ba2953424fdf1daf9081c3e9e383fab3823615
addison <addison@addisonredmond.com>
2024-09-21 18:58:02 -0500

    updated spell check to include more words

--- src/utils/spellCheck.ts
+++ src/utils/spellCheck.ts
@@ -1,20 +1,12 @@
 import SIX_LETTER_WORDS from "./six-letter-words";
 import words from "./words";
 import FOUR_LETTER_WORDS from "./four-letter-words";
+import dictionary from "./dictionary";
 
 export const checkSpelling = (word: string) => {
-  if (word.length === 4) {
-    if (FOUR_LETTER_WORDS.includes(word)) {
-      return true;
-    }
-  } else if (word.length === 5) {
-    if (words.includes(word)) {
-      return true;
-    }
-  } else if (word.length === 6) {
-    if (SIX_LETTER_WORDS.includes(word)) {
-      return true;
-    }
+  if (dictionary.includes(word)) {
+    return true;
+  } else {
+    return false;
   }
-  return false;
 };
