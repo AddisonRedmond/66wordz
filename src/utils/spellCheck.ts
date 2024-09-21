5484a23ba7a0c535917254b802513f2f8923ef9d
addison <addison@addisonredmond.com>
2024-09-21 18:59:20 -0500

    removed unused imports

--- src/utils/spellCheck.ts
+++ src/utils/spellCheck.ts
@@ -1,6 +1,3 @@
-import SIX_LETTER_WORDS from "./six-letter-words";
-import words from "./words";
-import FOUR_LETTER_WORDS from "./four-letter-words";
 import dictionary from "./dictionary";
 
 export const checkSpelling = (word: string) => {
