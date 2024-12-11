52b0726ff363372180d665d073e3e3fe207aea79
Addison Redmond <addison@addisonredmond.com>
2024-12-10 20:16:20 -0600

    updated headers for seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,44 @@ const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
     <ClerkProvider dynamic {...pageProps}>
       <Head>
         <title>66 Wordz</title>
-        <meta name="66 wordz" content="66wordz the game" />
-        <meta name="description" content="Battle royale word game. Play up to 66 players at a time" />
-        <meta name="keywords" content="Battle royale, brain game, competitive, multiplayer" />
+        <meta
+          name="description"
+          content="Join 66 Wordz, the ultimate battle royale word game! Compete with up to 66 players in real-time and test your vocabulary skills."
+        />
+        <meta
+          name="keywords"
+          content="word game, battle royale, multiplayer word game, brain games, competitive games, vocabulary game"
+        />
         <meta name="author" content="Addison Redmond" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        <meta
+          property="og:title"
+          content="66 Wordz | Battle Royale Word Game"
+        />
+        <meta
+          property="og:description"
+          content="Play 66 Wordz, the exciting battle royale word game! Compete with up to 66 players to win."
+        />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:url" content="https://66wordz.com" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="66 Wordz | Battle Royale Word Game"
+        />
+        <meta
+          name="twitter:description"
+          content="Test your vocabulary in 66 Wordz, the competitive battle royale word game!"
+        />
+        <meta name="twitter:image" content="/twitter-image.png" />
+        <meta name="twitter:creator" content="@66wordz" />
+
+        <link rel="canonical" href="https://66wordz.com" />
+
+        <link rel="icon" href="/favicon.png" />
 
         <link rel="icon" href="/favicon.png" />
       </Head>
