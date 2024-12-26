55257d3c141358ba194cf62d261fe53e2a5049ea
Addison Redmond <addison@addisonredmond.com>
2024-12-26 15:22:59 -0600

    updating join lobby styles

--- src/components/join-lobby.tsx
+++ src/components/join-lobby.tsx
@@ -7,7 +7,7 @@ const JoinLobby: React.FC<{
   return (
     <div
       style={{ borderRadius: `${joinLobby ? ".375rem" : "9999px"}` }}
-      className="cursor-pointer overflow-hidden bg-black p-2 text-white shadow-md outline outline-1 outline-zinc-300"
+      className="cursor-pointer overflow-hidden bg-zinc-900 p-2 text-white shadow-md outline outline-1 outline-zinc-300"
     >
       <div className="flex justify-between">
         <button onClick={() => setJoinLobby(true)} className="w-full">
@@ -16,26 +16,40 @@ const JoinLobby: React.FC<{
         {joinLobby && <button onClick={() => setJoinLobby(false)}>X</button>}
       </div>
       <m.div
-        initial={{ height: "0px", scale: 0, width: 0 }}
+        initial={{ height: "0px", scale: 0, width: 0, opacity: 0 }}
         animate={
           joinLobby
-            ? { height: "auto", scale: 1, width: "auto" }
-            : { scale: 0, height: "0px", width: 0 }
+            ? {
+                height: "200px",
+                scale: 1,
+                width: "260px",
+                padding: "1rem",
+                opacity: 1,
+              }
+            : { scale: 0, height: "0px", width: 0, opacity: 0 }
         }
         transition={{
           duration: 0.3,
-          ease: "easeInOut",
+          ease: "linear",
           type: "spring",
         }}
-        className="flex flex-col gap-y-2"
+        className="flex flex-col gap-y-4"
       >
-        <div className="flex flex-col">
-          <p className="bg-black">Lobby Id</p>
-          <input placeholder="lobby id"></input>
+        <div className="flex flex-col gap-y-1">
+          <p className="bg-zinc-900 text-sm">Lobby Id</p>
+          <input
+            placeholder="lobby id"
+            className="rounded-sm p-1 text-black"
+          ></input>
         </div>
-        <div className="flex flex-col">
-          <p className="bg-black">Password</p>
-          <input placeholder="password"></input>
+        <div className="flex flex-col gap-y-1">
+          <p className="bg-zinc-900 text-sm">Password</p>
+          <input
+            placeholder="password"
+            className="rounded-sm p-1 text-black"
+          ></input>
+
+          <button>Join</button>
         </div>
       </m.div>
     </div>
